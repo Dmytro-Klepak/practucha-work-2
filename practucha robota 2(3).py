5e077293f13d79b85a8a2df1bc383df296a7cab1
Dmytro-Klepak <dmitrooleksandrovic33@gmail.com>
2024-12-23 21:19:28 +0200

    Add files via upload

---
+++ practucha robota 2(3).py
@@ -0,0 +1,11 @@
+# Створення змінних
+message = "hello world"  # Текстова змінна
+name = "Дмитро"  # Текстова змінна
+surname = "Клепак"  # Текстова змінна
+age = 16  # Числова змінна
+
+# Виведення інформації про змінні
+print("Змінна 'message' має тип:", type(message), "і значення:", message)
+print("Змінна 'name' має тип:", type(name), "і значення:", name)
+print("Змінна 'surname' має тип:", type(surname), "і значення:", surname)
+print("Змінна 'age' має тип:", type(age), "і значення:", age)
